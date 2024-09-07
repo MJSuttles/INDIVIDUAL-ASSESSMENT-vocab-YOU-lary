bee640e23c6cf1f54c0ea6d339aa1cda5dba829c
MJSuttles <rolltiderolldad@gmail.com>
2024-09-07 14:22:11 -0500

    added promise and click event for each language

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,28 +1,38 @@
+/* eslint-disable prefer-destructuring */
 import firebase from 'firebase';
 import { signOut } from '../utils/auth';
-import { getCards, cardsByLanguage } from '../api/vocabData';
+import {
+  getCards, cardsByHTML, cardsByCSS, cardsByJavaScript
+} from '../api/vocabData';
 import { showCards, emptyCards } from '../pages/vocab';
 import addVocabForm from '../forms/vocabForm';
 
 const navigationEvents = () => {
   // LOGOUT BUTTON
-  console.warn("I'm about to listen to logout button");
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
 
   // ALL CARDS
-  console.warn("I'm about to listen to #createEntry");
   document.querySelector('#createEntry').addEventListener('click', () => {
     addVocabForm();
   });
 
-  // CARDS BY LANGUAGE
-  document.querySelector('#allLanguages').addEventListener('click', () => {
-    cardsByLanguage(`${firebase.auth().currentUser.uid}`).then(showCards);
+  // CARDS BY HTML
+  document.querySelector('#HTMLButton').addEventListener('click', () => {
+    cardsByHTML(`${firebase.auth().currentUser.uid}`).then(showCards);
+  });
+
+  // CARDS BY CSS
+  document.querySelector('#CSSButton').addEventListener('click', () => {
+    cardsByCSS(`${firebase.auth().currentUser.uid}`).then(showCards);
+  });
+
+  // CARDS BY JAVASCRIPT
+  document.querySelector('#JavaScriptButton').addEventListener('click', () => {
+    cardsByJavaScript(`${firebase.auth().currentUser.uid}`).then(showCards);
   });
 
   // DELETE CARD
-  console.warn("I'm about to listen to #allCards");
   document.querySelector('#allCards').addEventListener('click', () => {
     getCards(`${firebase.auth().currentUser.uid}`).then((cards) => {
       if (cards.length === 0) {
