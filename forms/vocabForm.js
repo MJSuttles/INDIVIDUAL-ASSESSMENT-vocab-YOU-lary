0f30b4c991e83ac088546f954cfef3e4fa407b8e
MJSuttles <rolltiderolldad@gmail.com>
2024-09-07 08:53:54 -0500

    changed order of definition and language on cards on DOM

--- forms/vocabForm.js
+++ forms/vocabForm.js
@@ -14,15 +14,15 @@ const addVocabForm = (obj = {}) => {
         <input type="text" class="form-control" id="definition" placeholder="Definition" required>
       </div>
       <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Language/Tech
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">HTML</a></li>
-    <li><a class="dropdown-item" href="#">CSS</a></li>
-    <li><a class="dropdown-item" href="#">JavaScript</a></li>
-  </ul>
-</div>
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Language/Tech
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">HTML</a></li>
+          <li><a id="language" class="dropdown-item" href="#">CSS</a></li>
+          <li><a class="dropdown-item" href="#">JavaScript</a></li>
+        </ul>
+      </div>
       <button type="submit" class="btn btn-primary mt-3">Submit</button>
     </form>`;
 
--- pages/vocab.js
+++ pages/vocab.js
@@ -15,8 +15,8 @@ const showCards = (array) => {
     <div class="card" style="width: 18rem;">
       <div class="card-body">
         <h3 class="card-title">${item.word}</h3>
-        <h3 class="card-subtitle mb-2 text-muted">${item.language}</h3>
         <h3 class="card-subtitle mb-2 text-muted">${item.definition}</h3>
+        <h3 class="card-subtitle mb-2 text-muted">${item.language}</h3>
         <hr>
         <i class="btn btn-success fas fa-eye" id="view-card-btn--${item.firebaseKey}"></i>
         <i class="fas fa-edit btn btn-info" id="update-vocab--${item.firebaseKey}"></i>
